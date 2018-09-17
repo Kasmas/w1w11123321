e244cccc7f12f0dbb1cde9d1402cfafbf2314b26
Kasmas <kasmas.kosmos@gmail.com>
2018-09-17 11:37:15 +0700

    Update Source.c

--- src/Source.c
+++ src/Source.c
@@ -28,27 +28,27 @@ int main() {
 		case '1':
 			printf("Enter addend: ");
 			scanf("%d", &second_operand);
-			y = addition(accumulator, second_operand);
+			accumulator = addition(accumulator, second_operand);
 			break;
 		case '2':
 			printf("Enter subtractor: ");
 			scanf("%d", &second_operand);
-			y = subtraction(accumulator, second_operand);
+			accumulator = subtraction(accumulator, second_operand);
 			break;
 		case '3':
 			printf("Enter multiplier: ");
 			scanf("%d", &second_operand);
-			y = multiplication(accumulator, second_operand);
+			accumulator = multiplication(accumulator, second_operand);
 			break;
 		case '4':
 			printf("Enter divide: ");
 			scanf("%d", &second_operand);
-			y = divide(accumulator, second_operand);
+			accumulatoraccumulator = divide(accumulator, second_operand);
 			break;
 		case '5':
 			printf("Enter exponent: ");
 			scanf("%d", &second_operand);
-			y = exponentation(accumulator, second_operand);
+			accumulator = exponentation(accumulator, second_operand);
 			break;
 		}
 
