87d3e8879c78a51a0c673e077b8b65cd4870d458
Kasmas <kasmas.kosmos@gmail.com>
2018-09-17 11:34:01 +0700

    Update Source.c

--- src/Source.c
+++ src/Source.c
@@ -4,7 +4,7 @@
 #include "Calc.h"
 
 int main() {
-	int y, second_operand;
+	int accumulator = 0, second_operand;
 	char key;
 	int a;
 
@@ -28,27 +28,27 @@ int main() {
 		case '1':
 			printf("Enter addend: ");
 			scanf("%d", &second_operand);
-			y = addition(y, second_operand);
+			y = addition(accumulator, second_operand);
 			break;
 		case '2':
 			printf("Enter subtractor: ");
 			scanf("%d", &second_operand);
-			y = subtraction(y, second_operand);
+			y = subtraction(accumulator, second_operand);
 			break;
 		case '3':
 			printf("Enter multiplier: ");
 			scanf("%d", &second_operand);
-			y = multiplication(y, second_operand);
+			y = multiplication(accumulator, second_operand);
 			break;
 		case '4':
 			printf("Enter divide: ");
 			scanf("%d", &second_operand);
-			y = divide(y, second_operand);
+			y = divide(accumulator, second_operand);
 			break;
 		case '5':
 			printf("Enter exponent: ");
 			scanf("%d", &second_operand);
-			y = exponentation(y, second_operand);
+			y = exponentation(accumulator, second_operand);
 			break;
 		}
 
